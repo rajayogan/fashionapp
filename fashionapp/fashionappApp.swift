d4d8c4e89d23d4028d0406decb47841af31d2829
Rajayogan <rajayogan27@gmail.com>
2025-05-05 22:41:30 +0530

    Initial Commit

---
+++ fashionapp/fashionappApp.swift
@@ -0,0 +1,17 @@
+//
+//  fashionappApp.swift
+//  fashionapp
+//
+//  Created by Rajayogan on 05/05/25.
+//
+
+import SwiftUI
+
+@main
+struct fashionappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ fashionappTests/fashionappTests.swift
@@ -0,0 +1,17 @@
+//
+//  fashionappTests.swift
+//  fashionappTests
+//
+//  Created by Rajayogan on 05/05/25.
+//
+
+import Testing
+@testable import fashionapp
+
+struct fashionappTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
